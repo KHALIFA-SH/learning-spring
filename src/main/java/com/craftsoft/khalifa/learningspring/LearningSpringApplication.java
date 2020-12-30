d980a81b6eaa81b54c3bb2dafb065d3e70eaa929
kelifa <kelifa>
2020-12-30 14:12:58 -0500

    Initial commit

---
+++ src/main/java/com/craftsoft/khalifa/learningspring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.craftsoft.khalifa.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/craftsoft/khalifa/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftsoft.khalifa.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
